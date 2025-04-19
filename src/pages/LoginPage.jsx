527161f5d8bfd4bfaa472c16a0a32a330661dc12
lee5031207 <lee5031207@naver.com>
2025-04-19 17:25:02 +0900

    logo change

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -2,7 +2,7 @@
 import React, { useState } from 'react';
 import axios from 'axios';
 import { useNavigate } from "react-router-dom";
-import { Box, FormControl, FormLabel, Input, Button, Stack, Text } from '@chakra-ui/react';
+import { Box, FormControl, FormLabel, Input, Button, Stack, Text, Flex } from '@chakra-ui/react';
 
 const LoginPage = () => {
 
@@ -40,38 +40,45 @@ const LoginPage = () => {
   };
 
   return (
-    <Box maxWidth="400px" mx="auto" mt="300px" p={4} boxShadow="lg" borderRadius="md" border="1px solid #ccc">
-      <Stack spacing={4}>
-        <img src="/appLogo.png" alt="PlanSync 로고" width={400} />
-        <FormControl>
-          <FormLabel htmlFor="loginId">로그인 ID</FormLabel>
-          <Input
-            id="loginId"
-            type="text"
-            value={loginId}
-            onChange={(e) => setLoginId(e.target.value)}
-            placeholder="ID를 입력하세요"
-          />
-        </FormControl>
+    <Flex
+      height="100vh"
+      alignItems="center"
+      justifyContent="center"
+      bg="gray.100"
+    >
+      <Box maxWidth="400px" mx="auto" m="auto" p={4} boxShadow="lg" borderRadius="md" border="1px solid #ccc">
+        <Stack spacing={4}>
+          <img src="/appLogo.png" alt="PlanSync 로고" width={400} />
+          <FormControl>
+            <FormLabel htmlFor="loginId">로그인 ID</FormLabel>
+            <Input
+              id="loginId"
+              type="text"
+              value={loginId}
+              onChange={(e) => setLoginId(e.target.value)}
+              placeholder="ID를 입력하세요"
+            />
+          </FormControl>
 
-        <FormControl>
-          <FormLabel htmlFor="password">비밀번호</FormLabel>
-          <Input
-            id="password"
-            type="password"
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-            placeholder="비밀번호를 입력하세요"
-          />
-        </FormControl>
+          <FormControl>
+            <FormLabel htmlFor="password">비밀번호</FormLabel>
+            <Input
+              id="password"
+              type="password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+              placeholder="비밀번호를 입력하세요"
+            />
+          </FormControl>
 
-        {error && <Text color="red.500" fontSize="sm">{error}</Text>}
+          {error && <Text color="red.500" fontSize="sm">{error}</Text>}
 
-        <Button bg="brand.200" color="white" _hover={{ bg: 'brand.300' }} onClick={handleLogin}>
-          로그인
-        </Button>
-      </Stack>
-    </Box>
+          <Button bg="brand.200" color="white" _hover={{ bg: 'brand.300' }} onClick={handleLogin}>
+            로그인
+          </Button>
+        </Stack>
+      </Box>
+    </Flex>
   );
 };
 
